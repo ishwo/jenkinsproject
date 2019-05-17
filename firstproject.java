4b52248d74ff1b74a07dbecfe9893c1fa237e826
Ishwo Rai <38336613+ishwo@users.noreply.github.com>
2019-05-17 16:26:56 -0400

    First Commit

---
+++ firstproject.java
@@ -0,0 +1,8 @@
+
+public class  firstproject {  
+    public static void main(String[] args) {  
+        for(int i=0; i < 20; i++) { 
+            System.out.println("This will printout numbers starting: " + i + "\n");
+        }
+    }
+}
